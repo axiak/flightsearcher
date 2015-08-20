d56de37470821aac4c4acd67125bcfe05ac5668c
Michael Axiak <mike@axiak.net>
2015-08-20 08:54:50 -0400

    more stuff

--- flightsearcher/main.py
+++ flightsearcher/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import json
+import random
+import sqlite3
 import datetime
 import itertools
-import random
+import collections
+
 import certifi
 import urllib3
-
 import requests
 
 from . import config
@@ -45,12 +47,23 @@ COST_PER_RUN = 1.0 # One dollar a day
 def main():
     slices = generate_all_slices()
     picked = pick_slices(slices, num_api_calls())
+
     for slice, _ in picked:
-        print json.dumps(get_response(slice))
+        air_fares = parse_results(slice, get_response(slice))
+        air_fares = pick_lowest_per_airline(air_fares)
+        print '\n'.join(map(str, air_fares))
         return
         
     print picked
 
+
+def pick_lowest_per_airline(air_fares):
+    result = {}
+    for air_fare in air_fares:
+        other = result.get(air_fare.airlines)
+        if other is None or other.total_cost > air_fare.total_cost:
+            result[air_fare.airlines] = air_fare
+    return result.values()
 
 def pick_slices(slices, num_to_pick):
     slices = list(slices)
@@ -90,12 +103,39 @@ def num_api_calls():
     return config.QPX_FREE_QUOTA + int(config.COST_PER_RUN / float(config.QPX_QUERY_COST))
 
 
-def parse_results(response):
+Airfare = collections.namedtuple('Airfare', 
+                                 'origin dest start_date end_date calculated_date total_cost total_mileage total_duration fare_calculation airlines'.split()
+                                 )
+
+
+def parse_results(input_slice, response):
+    airfares = []
     for trip_option in response.get('trips', {}).get('tripOption', ()):
         total_cost = float(trip_option.get('saleTotal', 'USD0')[3:])
         slices = trip_option.get('slice', {})
         total_mileage = get_mileage(slices)
-        total_duration = sum(slice['duration'] for slice in 
+        total_duration = sum(slice['duration'] for slice in slices)
+        fare_calculation = trip_option['pricing'][0]['fareCalculation']
+        
+        airlines = sorted(set(segment["flight"]["carrier"] for slice in slices
+                              for segment in slice["segment"]))
+        airfares.append(Airfare(
+                input_slice[0]['origin'],
+                input_slice[0]['destination'],
+                parse_date(input_slice[0]['date']),
+                parse_date(input_slice[1]['date']) ,
+                datetime.date.today(),
+                total_cost,
+                total_mileage,
+                total_duration,
+                fare_calculation,
+                ",".join(airlines)
+        ))
+    return airfares
+
+def parse_date(input_str):
+    return datetime.datetime.strptime(input_str, "%Y-%m-%d").date()
+
 
 def get_mileage(slices):
     mileage = 0
@@ -168,6 +208,8 @@ def generate_slices(from_, to_):
 
 
 def get_response(slices):
+    with open('/tmp/output') as f:
+        return json.load(f)
     request = {
         "request": {
             "passengers": {
